de114d94a4ae918fc7ea8f96e574b41e2da53284
britt-theunis <r0785522@student.thomasmore.be>
2021-02-22 17:02:38 +0100

    huisstijl

---
+++ resources/views/shared/navigation.blade.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-fixed-top navbar-expand-lg flex-column">
+    <div id="lijn1">
+        <a class="navbar-brand" href="/">Onkostenportaal</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsNav">
+            <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse shadow-sm" id="collapsNav">
+{{--                Navigatie voor iedere user--}}
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Fietsvergoeding aanvragen</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">Laptopvergoeding aanvragen</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">Diverse vergoeding aanvragen</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">Mijn aanvragen</a>
+            </li>
+{{--                    Navigatie voor docent--}}
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#!" data-toggle="dropdown">Rik Rikken (d)</a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a class="dropdown-item" href="/">Uitloggen</a>
+                    <a class="dropdown-item" href="/">Wachtwoord aanpassen</a>
+                </div>
+            </li>
+{{--                    Navigatie voor kostenplaatsverantwoordelijke --}}
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#!" data-toggle="dropdown">John Doe (k)</a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a class="dropdown-item" href="/">Uitloggen</a>
+                    <a class="dropdown-item" href="/">Wachtwoord aanpassen</a>
+                    <a class="dropdown-item" href="/">Aanvragen beheren</a>
+                    <a class="dropdown-item" href="/">Kostenplaatsen vergelijken</a>
+                </div>
+            </li>
+{{--                    Navigatie voor financieel medewerker--}}
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#!" data-toggle="dropdown">Jane Doe (f)</a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a class="dropdown-item" href="/">Uitloggen</a>
+                    <a class="dropdown-item" href="/">Wachtwoord aanpassen</a>
+                    <a class="dropdown-item" href="/">Vergoedingen behandelen</a>
+                    <a class="dropdown-item" href="/">Gebruikers beheren</a>
+                    <a class="dropdown-item" href="/">Kostenplaatsen beheren</a>
+                    <a class="dropdown-item" href="/">Mailteksten beheren</a>
+                    <a class="dropdown-item" href="/">Tarieven beheren</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+
