2cb4d2e1f52a6c18a7382da4acd46a062e63b4b4
Nanotect <wilferspaepen@gmail.com>
2021-03-15 17:04:16 +0100

    Fixed new form not closing

--- resources/views/financial_employee/cost_centers/cost_center.blade.php
+++ resources/views/financial_employee/cost_centers/cost_center.blade.php
@@ -60,7 +60,6 @@
 @endsection
 
 @section('script_after')
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
     <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
     <script>
@@ -251,6 +250,8 @@
             $("#budget_input").val("");
 
             $("#active_input").prop("checked", false);
+
+            $("#cost_center_form_modal").modal('hide');
         }
 
         function add_cost_center(cost_center){
