a9ab863135ed79cea4008bbaf44927f03f5112e0
britt-theunis <r0785522@student.thomasmore.be>
2021-04-19 17:26:38 +0200

    homepagina toegevoegd

--- resources/views/financial_employee/parameters.blade.php
+++ resources/views/financial_employee/parameters.blade.php
@@ -100,7 +100,8 @@
 @endsection
 
 @section('script_after')
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+
+
     <script>
         $(document).ready(function(){
 
