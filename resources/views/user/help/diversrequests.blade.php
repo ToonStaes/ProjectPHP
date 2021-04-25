5a6f1bfbb4ce3f71f608ecd423bcd2b6eb9f430d
jornemarx <r0781277@student.thomasmore.be>
2021-04-25 21:05:19 +0200

    Diverse vergoedingen hulp pagina

--- resources/views/user/help/diversrequests.blade.php
+++ resources/views/user/help/diversrequests.blade.php
@@ -4,30 +4,33 @@
 
 @section('main')
     <div class="help container">
-        <h1>Help - laptopvergoeding</h1>
+        <h1>Help - diverse aanvragen</h1>
 
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-sm-12">
-                    <img src="\assets\images\LaptopScreenshot.png" alt="Screenshot website" style="width: 100%">
+                    <img src="\assets\images\DiverseScreenshot.png" alt="Screenshot website" style="width: 100%">
                 </div>
                 <div class="col-md-4 col-sm-12">
-                    <h4>Een laptopvergoeding aanvragen gaat als volgt:</h4>
+                    <h4>Diverse vergoedingen aanvragen gaat als volgt:</h4>
                     <ol class="">
                         <li>
-                            <p>Vul het bedrag van de aankoop in in Euro.</p>
+                            <p>Voeg met de knop bovenaan zoveel kosten toe als gewenst.</p>
                         </li>
                         <li>
-                            <p>Geef de reden van de aanvraag. Dit moet slechts een korte tekst zijn.</p>
+                            <p>Begin met het selecteren van een kostenplaats. <em>Dit is de kostenplaats voor alle kosten die je toevoegt.</em></p>
                         </li>
                         <li>
-                            <p>Selecteer de datum van de aankoop. Deze datum kan geen dag zijn na vandaag.</p>
+                            <p>Geef vervolgens een verklaring in voor de aanvraag. Ook deze reden geld voor elke kost.</p>
                         </li>
                         <li>
-                            <p>Upload een bestand om de aankoop te bewijzen door op de knop 'bladeren' te klikken en een bestand op uw apparaat te kiezen. Dit kan in om het even welk formaat zijn, maar de voorkeur gaat uit naar PDF.</p>
+                            <p>Indien het gaat over een autovergoeding, klik dan op de slider. Er verschijnt een vakje om het aantal kilometers in te geven. Voor de rest hoef je bij een autovergoeding niets in te vullen.</p>
                         </li>
                         <li>
-                            <p>Wanneer u klaar bent met het invullen van de gegevens, klikt u op 'aanvraag bevestigen'. U krijgt bovenaan het formulier een bericht dat u laat weten of de aanvraag succesvol is. Indien er toch iets fout loopt krijgt u te zien wat het probleem is.</p>
+                            <p>Indien het niet gaat over een autovergoeding vul je eerst het bedrag van de kost in. Vervolgens geef je de datum in van de aankoop. Deze datum kan niet na vandaag zijn. Als laatste klik je nog op "bladeren" en selecteer je een bestand op je toestel als bewijs. Je kan nog extra bestanden toevoegen door op het "+" knopje te klikken.</p>
+                        </li>
+                        <li>
+                            <p>Wanneer u klaar bent met het invullen van de gegevens, klikt u op 'indienen'. U krijgt bovenaan het formulier een bericht dat u laat weten of de aanvraag succesvol is. Indien er toch iets fout loopt krijgt u te zien wat het probleem is.</p>
                         </li>
                     </ol>
                 </div>
