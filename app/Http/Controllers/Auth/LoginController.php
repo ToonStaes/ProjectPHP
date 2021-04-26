7a338758e72e073b23279422c4886a50b7891a4f
Arne Hus <r0793284@student.thomasmore.be>
2021-04-26 17:00:00 +0200

    Nieuwe laptop reimbursement aanmaken als de vorige langer dan een jaar geleden is

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,8 +7,10 @@ use App\Laptop_invoice;
 use App\Laptop_reimbursement;
 use App\Parameter;
 use App\Providers\RouteServiceProvider;
+use Facades\App\Helpers\Json;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
@@ -54,12 +56,13 @@ class LoginController extends Controller
         $verantwoordelijke = Parameter::with('cost_center.user')->findOrFail(4)->cost_center->user->id;
         if ($user->id == $verantwoordelijke){
             $laptop_invoices = Laptop_invoice::with('laptop_reimbursements')->withCount('laptop_reimbursements')->get();
+
             foreach ($laptop_invoices as $invoice){
-                if ($invoice->laptop_reimbursements_count != 4){
+                if ($invoice->laptop_reimbursements_count < 4 && $invoice->laptop_reimbursements_count != 0){
                     $latest_reimbursement = $invoice->laptop_reimbursements->last();
 
                     //Als laptop reimbursement langer dan een jaar geleden, maak een nieuwe aan
-                    if ($latest_reimbursement->payment_date < strtotime('-1 year')){
+                    if (strtotime($latest_reimbursement->payment_date)<strtotime('-1 year') && $latest_reimbursement->payment_date != null){
                         $NewLapReimb = new Laptop_reimbursement();
                         $NewLapReimb->laptop_invoice_id = $invoice->id;
                         $NewLapReimb->user_id_Cost_center_manager = $user->id;
@@ -70,10 +73,10 @@ class LoginController extends Controller
             }
         }
 
-        if ($user->changedPassword) {
-            return redirect('/');
-        } else {
-            return redirect('user/firstPassword');
-        }
+//        if ($user->changedPassword) {
+//            return redirect('/');
+//        } else {
+//            return redirect('user/firstPassword');
+//        }
     }
 }
