51b23c5eb02ca5f0c020508b1fa7439938bd81f3
britt-theunis <r0785522@student.thomasmore.be>
2021-05-12 13:11:26 +0200

    toevoeging dat het gaat om een proefproject

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <h1>Welkom op het onkostenportaal van Thomas More!</h1>
         <p>Als Thomas More medewerker is de kans zeer groot dat u wel eens onkosten maakt voor uw beroep, zoals een stagebezoek, een laptopaankoop... Ook is er de mogelijkheid om in deze webapplicatie een vergoeding aan te vragen wanneer u zich met de fiets verplaatst naar het werk.</p>
         <p>Indien u nog niet over inloggegevens beschikt, contacteer dan de financiële dienst. Beschikbaar van maandag tot en met vrijdag, vanaf 8u tot 18u, op het telefoonnummer 015 36 91 00.</p>
+        <p class="font-italic">Deze webapplicatie is ontwikkeld door vijf studenten Toegepaste Informatica van Thomas More te Geel. Het onkostenportaal is een <b>proefproject</b> voor het opleidingsonderdeel "Project PHP".</p>
     @else
     @endguest
 
